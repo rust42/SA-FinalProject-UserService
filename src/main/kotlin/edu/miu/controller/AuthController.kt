5ce839b54f1f3a1c39916c577312fa6d355cb643
Sandeep <sndpkrl007@gmail.com>
2022-10-22 04:11:08 -0500

    images

--- src/main/kotlin/edu/miu/controller/AuthController.kt
+++ src/main/kotlin/edu/miu/controller/AuthController.kt
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -18,7 +17,7 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin
-class AuthController() {
+class AuthController {
     @Autowired
     private lateinit var userService: UserService
     @PostMapping("/signup")
